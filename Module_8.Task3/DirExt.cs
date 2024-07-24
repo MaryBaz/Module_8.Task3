1b78813f3d874b5dde28db8394445d97b8d2acec
Name <email@example.ru>
2024-07-24 21:00:45 +0500

    Добавьте файлы проекта.

---
+++ Module_8.Task3/DirExt.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_8.Task3
+{
+    public static class DirExt
+    {
+        public static long FolderSize(this DirectoryInfo folder)
+        {
+            long size = 0;
+            try
+            {
+                FileInfo[] files = folder.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    size += file.Length;
+                }
+
+                foreach (DirectoryInfo directory in folder.GetDirectories())
+                {
+                    //Console.WriteLine($"Размер : {directory} = {size} байт");
+                    size += FolderSize(directory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            return size;
+        }
+        public static long DeleteOldFiles(this DirectoryInfo folder)
+        {
+            long size = 0;
+
+            try
+            {
+                foreach (FileInfo file in folder.GetFiles())
+                {
+                    if (DateTime.Now - file.LastAccessTime >= TimeSpan.FromMinutes(30))
+                    {
+                        size += file.Length;
+                        file.Delete();
+                    }
+                }
+                foreach (DirectoryInfo directory in folder.GetDirectories())
+                {
+                    //Console.ForegroundColor = ConsoleColor.Cyan;
+                    //Console.BackgroundColor = ConsoleColor.Black;
+                    //Console.WriteLine($"Директория: {directory} размер: {size} ");
+                    size += DeleteOldFiles(directory);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(e.Message);
+            }
+
+            return size;
+        }
+
+    }
+
+}
---
+++ Module_8.Task3/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Module_8.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Укажите директорию: ");
+            //string DirPath = Console.ReadLine();
+            string DirPath = @"D:\Temp";
+            if (Directory.Exists(DirPath))
+            {
+                DirectoryInfo dir = new DirectoryInfo(DirPath);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine($"Исходный размер папки: {dir.FolderSize()} байт");
+                Console.WriteLine($"Освобождено: {dir.DeleteOldFiles()} байт");
+                DeleteFolders(DirPath);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine($"Текущий размер папки: {dir.FolderSize()} байт");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Директории {DirPath} не найдено!");
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+        // <summary>
+        // Рекурсивное удаление каталогов
+        // </summary>
+        // <param name="folder"></param>
+        public static void DeleteFolders(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                return;
+            }
+            try
+            {
+                foreach (string directory in Directory.GetDirectories(folder))
+                {
+                    //Console.ForegroundColor = ConsoleColor.Cyan;
+                    //Console.BackgroundColor = ConsoleColor.Black;
+                    //Console.WriteLine($"Директория: {directory} дата использования: {Directory.GetLastAccessTime(directory)} ");
+                    if ((DateTime.Now - Directory.GetLastAccessTime(directory)) >= TimeSpan.FromMinutes(30)
+                        && Directory.GetDirectories(directory).Length == 0
+                        && Directory.GetFiles(directory).Length == 0
+                       )
+                    {
+                        Directory.Delete(directory);
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Директория {directory} удалена!");
+                    }
+                    DeleteFolders(directory);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
